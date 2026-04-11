87bb2b12e7c752d3dca0cb9837d19bbc5cd3ee28
SeBAs1010CR <sazofeifa@estudiantec.cr>
2026-04-10 21:49:49 -0600

    refactor: cleanup and final tweaks

--- include/sorter.h
+++ include/sorter.h
@@ -5,9 +5,10 @@
 
 class Sorter
 {
+private:
+    static void merge(PagedArray& arr, long long left, long long mid, long long right);
 public:
-    static void selectionSort(PagedArray& arr, long long n);
-    static void insertionSort(PagedArray& arr, long long n);
+    static void mergeSort(PagedArray& arr, long long left, long long right);
     static void quickSort(PagedArray& arr, long long low, long long high);
 };
 
